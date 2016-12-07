798895786f2477db0df096c7df28e509fc44ed0a
yuichi10 <y_084743@yahoo.co.jp>
2016-12-07 23:54:21 +0900

    connect db

--- src/main.go
+++ src/main.go
@@ -5,8 +5,12 @@ import (
 	"log"
 	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 )
+
+var db *gorm.DB
 
 func EnvLoad() {
 	err := godotenv.Load()
@@ -16,8 +20,19 @@ func EnvLoad() {
 	}
 }
 
+func openDB() {
+	var err error
+	sql := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	db, err = gorm.Open(os.Getenv("DB"), sql)
+	if err != nil {
+		fmt.Printf("エラー%v\n", err)
+		return
+	}
+}
+
 func main() {
 	EnvLoad()
+	openDB()
 	fmt.Print(os.Getenv("DB_USER"))
 	fmt.Println("hello go")
 }
