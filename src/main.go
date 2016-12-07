c036e54469d3ef5ecaded4d313e9b1f26b6df074
yuichi10 <y_084743@yahoo.co.jp>
2016-12-07 23:25:09 +0900

    load env

--- src/main.go
+++ src/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func EnvLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal("Err loading .env")
+	}
+}
+
 func main() {
+	EnvLoad()
+	fmt.Print(os.Getenv("DB_USER"))
 	fmt.Println("hello go")
 }
